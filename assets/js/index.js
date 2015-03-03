61792c91177f7c0abe78461bc29a04a8d28c7b07
45kb <filippo.oretti@gmail.com>
2015-03-03 10:23:11 +0100

    Trying updating scope in expressions

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,5 +6,10 @@
   angular.module('720kb', [
     'ngRoute',
     '720kb.fx'
-  ]);
+  ])
+  .controller('Ctrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $timeout(function () {
+      $scope.x = 5;
+    }, 2000);
+  }]);
 }(angular));
